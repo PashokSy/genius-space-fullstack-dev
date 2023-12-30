f75bac9bc6c3fbfa5268fddf01a050cf49a7a78e
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-12-30 17:09:38 +0200

    feat: js lesson 4
    module 24 lection

---
+++ lectures/java_script/lessons/lesson_4/js/script.js
@@ -0,0 +1,30 @@
+//functions
+
+// declaration
+// function showMessage() {
+//   console.log('Hello');
+// }
+// showMessage();
+// function showMessage(name, city) {
+//   console.log(`Hello, my name is ${name}, I'm from ${city}`);
+// }
+// showMessage('Nata', 'Kyiv');
+
+// expression
+// const sayHi = function () {
+//   console.log('Hi');
+// };
+// sayHi();
+// const userName = 'Ann';
+// const sayHi = function () {
+//   const message = 'Hello, my name is ' + userName;
+//   console.log(message);
+// };
+// sayHi();
+
+// return
+// function sum(a, b) {
+//   return a + b;
+// }
+// const sum = (a, b) => a + b;
+// console.log(sum(2, 2));
