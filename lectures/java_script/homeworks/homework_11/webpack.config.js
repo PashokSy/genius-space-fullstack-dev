8c20449a7c27e35ca7b37b78179c4d23426c3a93
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-10 20:48:20 +0200

    feat: js homeworks 10,11

---
+++ lectures/java_script/homeworks/homework_11/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 5000,
+  },
+};
