eb1de33a9f2204b6d4a771d8e8f2cc807e6741da
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-11 23:31:36 +0200

    feat: js lesson 12
    module 32

---
+++ lectures/java_script/lessons/lesson_12/js/script.js
@@ -0,0 +1,26 @@
+// const hello = () => {
+//   console.log('hello');
+// };
+// const timeout = setTimeout(hello, 1000);
+// clearTimeout(timeout);
+
+// const hello = () => {
+//   console.log('hello');
+// };
+// const interval = setInterval(hello, 1000);
+// clearInterval(interval);
+
+// const a = {
+//   classic() {
+//     setTimeout(function () {
+//       console.log(this);
+//     });
+//   },
+//   arr() {
+//     setTimeout(() => {
+//       console.log(this);
+//     });
+//   },
+// };
+// a.classic()
+// a.arr()
