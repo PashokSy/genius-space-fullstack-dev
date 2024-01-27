f0efad366ae3977c15871a5bafc611b818bb1d4a
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-01-27 18:21:16 +0200

    feat: react lesson 10
    module 45

--- lectures/react/lessons/src/App.js
+++ lectures/react/lessons/src/App.js
@@ -1,13 +1,27 @@
-import { Fetch } from './9_lesson_components/Fetch';
+import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import { Home } from './10_lesson_components/Home';
+import { Contact } from './10_lesson_components/Contact';
+import { About } from './10_lesson_components/About';
+import { NotFoundPage } from './10_lesson_components/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Fetch/>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/about">About</Link>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/404" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
