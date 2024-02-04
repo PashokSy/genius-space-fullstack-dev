272b7c76916cd062240dbea930d3fdc3e2d2d7bc
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-02-04 17:03:02 +0200

    feat: react native lesson 4
    add navigation menu
    
    docs: install react redux

--- lectures/react-native/lessons/App.tsx
+++ lectures/react-native/lessons/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faHome, faCalendarDays} from '@fortawesome/free-solid-svg-icons';
+
 import HomePage from './src/pages/HomePage';
+import Calendar from './src/pages/Calendar';
+
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <HomePage />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarActiveTintColor: '#0057b8',
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#ffd600',
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color}) => (
+              <FontAwesomeIcon icon={faHome} color={color} />
+            ),
+          }}
+          component={HomePage}
+        />
+        <Tab.Screen
+          name="Calendar"
+          options={{
+            tabBarLabel: 'Calendar',
+            tabBarIcon: ({color}) => (
+              <FontAwesomeIcon icon={faCalendarDays} color={color} />
+            ),
+          }}
+          component={Calendar}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
