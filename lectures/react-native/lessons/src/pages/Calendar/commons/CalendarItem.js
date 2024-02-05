cad8cfdca0560b3fe48785f9d878910fcf7bdd29
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-02-05 14:56:18 +0200

    refactor: react native lesson 5
    add react native calendar

--- lectures/react-native/lessons/src/pages/Calendar/commons/CalendarItem.js
+++ lectures/react-native/lessons/src/pages/Calendar/commons/CalendarItem.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
 
 const CalendarItem = () => {
+  const [selected, setSelected] = useState('');
+
   return (
     <SafeAreaView style={styles.calendarBlock}>
       <Text style={styles.calendarDay}>4 лютого</Text>
-      <View style={styles.calendar}></View>
+      <View style={styles.calendar}>
+        <Calendar
+          onDayPress={day => {
+            setSelected(day.dateString);
+          }}
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedDotColor: 'orange',
+            },
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
