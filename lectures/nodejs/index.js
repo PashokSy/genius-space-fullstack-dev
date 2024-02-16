5164e4e0726c386ed0530f782643f9e06a42b628
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-02-16 15:34:15 +0200

    feat: nodejs lesson 2
    module 57

--- lectures/nodejs/index.js
+++ lectures/nodejs/index.js
@@ -1,12 +1,70 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = 3000;
+
+let tasks = [
+  {
+    id: 1,
+    text: "Read a book",
+  },
+  {
+    id: 2,
+    text: "Walk a dog",
+  },
+  {
+    id: 3,
+    text: "Cook meal",
+  },
+  {
+    id: 4,
+    text: "Call mom",
+  },
+];
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Express");
 });
 
+app.get("/tasks", (req, res) => {
+  res.status(200).json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const newTask = req.body;
+
+  const newTaskId = tasks.at(-1).id + 1;
+  tasks.push({ id: newTaskId, text: newTask.text });
+
+  res.status(201).json(tasks);
+});
+
+app.put("/tasks/:id", (req, res) => {
+  const updatedTask = req.body;
+  const taskId = parseInt(req.params.id);
+
+  const foundTask = tasks.find((task) => task.id === taskId);
+
+  if (!foundTask) {
+    res.status(404).json({ message: "Task not found" });
+  }
+
+  foundTask.text = updatedTask.text;
+
+  res.status(200).json(foundTask);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+
+  tasks = tasks.filter((t) => t.id !== taskId);
+
+  res.status(204).json(tasks);
+});
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
