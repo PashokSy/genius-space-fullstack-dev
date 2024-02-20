70f00078c98e8500e7365c8685390d5fcaaa22d8
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-02-20 18:20:00 +0200

    feat: nodejs lesson 4
    module 59

--- lectures/nodejs/index.js
+++ lectures/nodejs/index.js
@@ -5,7 +5,7 @@ const app = express();
 const sqlite3 = require("sqlite3").verbose();
 
 const { connectDB } = require("./config/db");
-const Task = require("./models/tasksModel");
+const { Task } = require("./models/tasksModel");
 
 const dbName = "tasks.db";
 const port = process.env.PORT || 5000;
@@ -14,14 +14,10 @@ const db = new sqlite3.Database(dbName);
 
 app.use(bodyParser.json());
 
-const checkExist = (id, res) => {
-  db.all("SELECT * FROM tasks", (err, row) => {
-    serverError(err, res);
-
-    if (!row) {
-      return res.status(404).json({ message: "Task not found" });
-    }
-  });
+const checkExist = (task, res, err) => {
+  if (!task) {
+    return res.status(404).json({ message: err ?? "Помилка запиту" });
+  }
 };
 
 const serverError = (err, res) => {
@@ -32,54 +28,76 @@ app.get("/", (req, res) => {
   res.send("Hello, Express");
 });
 
-app.get("/tasks", (req, res) => {
-  db.all("SELECT * FROM tasks", (err, row) => {
-    serverError(err, res);
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
 
-    res.status(200).json(row);
-  });
+    return res.status(200).json(tasks);
+  } catch (error) {
+    console.error("Task creation error: ", error);
+    serverError(error, res);
+  }
 });
 
-app.get("/tasks/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findById(id);
 
-  db.get("SELECT * FROM tasks WHERE id = ?", id, (err, row) => {
-    serverError(err, res);
-    checkExist(row, res);
-
-    return res.status(200).json(row);
-  });
+    return res.status(200).json(task);
+  } catch (error) {
+    console.error("Task creation error: ", error);
+    serverError(error, res);
+  }
 });
 
-app.post("/tasks", (req, res) => {
-  const newTask = req.body;
+app.post("/tasks", async (req, res) => {
+  try {
+    const newTask = req.body;
 
-  db.run("INSERT INTO tasks (text) VALUES (?)", [newTask.text], (err) => {
-    serverError(err, res);
+    const task = await Task.create({ text: newTask.text });
 
-    res.status(201).send();
-  });
+    checkExist(task, res, "Завдання не створене");
+
+    return res.status(201).json(task);
+  } catch (error) {
+    console.error("Task creation error: ", error);
+    serverError(error, res);
+  }
 });
 
-app.put("/tasks/:id", (req, res) => {
-  const { text } = req.body;
-  const id = parseInt(req.params.id);
+app.put("/tasks/:id", async (req, res) => {
+  try {
+    const { text, isCompleted } = req.body;
+    const id = req.params.id;
 
-  db.run("UPDATE tasks SET text = ? WHERE id = ?", [text, id], (err) => {
-    serverError(err, res);
+    const task = await Task.findByIdAndUpdate(
+      id,
+      {
+        text,
+        isCompleted,
+      },
+      { new: true }
+    );
 
-    return res.status(200).json({ id, text });
-  });
+    return res.status(201).json(task);
+  } catch (error) {
+    console.error("Task creation error: ", error);
+    serverError(error, res);
+  }
 });
 
-app.delete("/tasks/:id", (req, res) => {
-  const id = parseInt(req.params.id);
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
 
-  db.run("DELETE FROM tasks WHERE id = ?", id, (err) => {
-    serverError(err, res);
+    await Task.findByIdAndDelete(id);
 
     return res.status(204).send();
-  });
+  } catch (error) {
+    console.error("Task creation error: ", error);
+    serverError(error, res);
+  }
 });
 
 app.listen(port, () => {
---
+++ lectures/nodejs/models/tasksModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const tasksSchema = new mongoose.Schema({
+  text: {
+    type: "string",
+    require: [true, "Task description is required"],
+  },
+  isCompleted: {
+    type: "boolean",
+    default: false,
+  },
+});
+
+const Task = mongoose.model("Task", tasksSchema);
+
+module.exports = {
+  Task,
+};
