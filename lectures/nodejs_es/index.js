81b79efb6db53e9936c728aeb26ddb7cfdca79b0
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-03-04 17:56:40 +0200

    feat: nodejs lesson 8
    modules

---
+++ lectures/nodejs_es/index.js
@@ -0,0 +1,7 @@
+import util, { add, sub } from "./utils.js";
+
+console.log(add(2, 3));
+console.log(sub(2, 3));
+console.log(util.mul(2, 3));
+
+console.log(12.0 / 4.0);
---
+++ lectures/nodejs_es/utils.js
@@ -0,0 +1,9 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const sub = (a, b) => a - b;
+
+const mul = (a, b) => a * b;
+
+export default { mul };
