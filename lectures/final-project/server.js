eaf47e36f88e120b0d0dd766f3e43231a686011f
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-03-08 17:32:20 +0200

    feat: final project
    mongodb connect
    prettier fix

--- lectures/final-project/server.js
+++ lectures/final-project/server.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import 'dotenv/config.js';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-const app = express()
-const port = 3000
+import connectDB from './config/db.js';
+
+const app = express();
+const port = 3000;
 
 //middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.listen(port, () => {
-  console.log(`Server - http://localhost:${port}`)
-})
+  try {
+    connectDB(process.env.MONGO_URI);
+    console.log(`Server - http://localhost:${port}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
