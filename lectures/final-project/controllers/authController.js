c6651d180293c5dd6ee5172602deefef7a492593
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2024-03-11 17:58:32 +0200

    feat: fp authController

---
+++ lectures/final-project/controllers/authController.js
@@ -0,0 +1,53 @@
+import bcrypt from 'bcrypt';
+
+import User from '../models/userModel.js';
+
+export const signIn = async (req, res) => {
+  try {
+    const { username, email, role } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hash,
+      role,
+    });
+
+    return res.status(201).json({ firstName, lastName, email });
+  } catch (error) {
+    console.error('Register error: ', error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      return res.status(400).json({
+        message: 'Invalid password or email',
+      });
+    }
+
+    return res
+      .status(201)
+      .json({ username: user.username, email: user.email, role: user.role });
+  } catch (error) {
+    console.error('Register error: ', error);
+    res.status(500).json({ message: error.message });
+  }
+};
